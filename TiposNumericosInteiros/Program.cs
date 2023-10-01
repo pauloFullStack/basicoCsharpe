af6a921ac621ad04a980f45956818a2b4851b9a2
Paulo <paulofullstack@hotmail.com>
2023-10-01 14:43:40 -0300

    Adicionar arquivos de projeto.

---
+++ TiposNumericosInteiros/Program.cs
@@ -0,0 +1,20 @@
+﻿// São tipos por valor e dão suporte a operadores aritméticos de comparação e de igualdade
+
+Console.WriteLine("Declarando variáveis numéricas");
+Console.WriteLine();
+
+// Declarando variáveis
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -2147483647;
+valor3 = 1000;
+uint valor4 = 2147483647;
+long valor5 = -21474836489;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+
+Console.ReadLine();
---
+++ TiposNumerosPontoFlutuante/Program.cs
@@ -0,0 +1,28 @@
+﻿/* - Representam números reais  
+ * - São tipos de valor armazenados na stack
+ * - O valor padrão dos tipos de dados ponto flutuante é zero (0)
+ * - Dão suporte a operadores aritméticos de comparação (>,<,>=,<=,!=) e de igualdade (==)
+ * - O tipo double é usado para cálculos científicos e o decimal para cálculos financeiros
+ * - São tipos que podemos inicializar  usando literais (f-F, d-D, m-M)
+ */
+
+Console.WriteLine("Hello, World!");
+
+// Representações:
+
+// double com alias => 'double' => 'palavra-chave/tipo C#':
+double valorD1 = 12.4;
+// Double com Tipo .NET => 'System.Double'
+System.Double valorD2 = 12.4;
+
+
+// float com alias => 'float' => 'palavra-chave/tipo C# ':
+float valorF1 = 12.4F;
+// float com Tipo .NET => 'System.Single'
+System.Single valorF2 = 12.4f;
+
+
+// decimal com alias => 'decimal' => 'palavra-chave/tipo C#':
+decimal valorDC1 = 12.4M;
+// decimal com Tipo .NET => 'System.Single'
+System.Decimal valorDC2 = 12.4m;
